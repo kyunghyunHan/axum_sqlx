75617cd5173a7aaadd7e877cffd288d6eee83427
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-09-11 22:01:53 +0900

    feat:secession

--- src/router/auth.rs
+++ src/router/auth.rs
@@ -10,7 +10,7 @@ pub async fn get_users() -> JsonResponse<Value> {
     let users = sqlx::query("SELECT id, user_id,user_pw FROM users")
         .fetch_all(&connection)
         .await
-        .expect("Failed to fetch users.");
+        .expect("Failedq to fetch users.");
 
     let users: Vec<User> = users
         .into_iter()
@@ -50,9 +50,46 @@ pub async fn join(input: Json<JoinUser>) -> JsonResponse<Value> {
     let connection: sqlx::Pool<sqlx::Postgres> = connection().await;
 
     let query =
-        sqlx::query("SELECT id, user_id, user_pw FROM users WHERE user_id = $1 AND user_pw = $2")
+        sqlx::query("SELECT id, user_id FROM users WHERE user_id = $1 AND user_pw = $2")
             .bind(&input.user_id) // user_id 값 바인딩
-            .bind(&input.user_pw) // user_pw 값 바인딩
+            .fetch_all(&connection)
+            .await
+            .expect("Failed to fetch users.");
+
+    let result = query.is_empty();
+    println!("{}", result);
+
+    match result {
+        true => {
+            let result = sqlx::query("INSERT INTO users (user_id, user_pw) VALUES ($1, $2)")
+                .bind(&input.user_id) // user_id 값 바인딩
+                .bind(&input.user_pw) // user_pw 값 바인딩
+                .execute(&connection) // 쿼리 실행
+                .await
+                .expect("Failed to insert user");
+
+            match result {
+                exec_result => {
+                    if exec_result.rows_affected() > 0 {
+                        JsonResponse(json!({ "result": true}))
+                        // 추가 작업이 필요할 경우 여기에 작성
+                    } else {
+                        JsonResponse(json!({ "result":false,"msg":"You are already registered"}))
+                    }
+                }
+            }
+        }
+        false => JsonResponse(json!({ "result":false,"msg":"You are already registered"})),
+    }
+}
+
+
+pub async fn secession(input: Json<JoinUser>) -> JsonResponse<Value> {
+    let connection: sqlx::Pool<sqlx::Postgres> = connection().await;
+
+    let query =
+        sqlx::query("SELECT user_id FROM users WHERE user_id = $1 AND user_pw = $2")
+            .bind(&input.user_id) // user_id 값 바인딩
             .fetch_all(&connection)
             .await
             .expect("Failed to fetch users.");
