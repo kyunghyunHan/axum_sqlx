82bab5d15ef34925cb47cfbe4b6cc4689de331f9
kyunghyunHan <ekfqlwcjswl@naver.com>
2024-10-04 17:41:46 +0900

    feat:cors add

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,37 @@
 mod db;
 mod model;
 mod router;
-
-use crate::router::{
-    // auth::{get_users, join, login, secession, update_user},
-    // test::test,
+use axum::{
+    response::{Html, Json as JsonResponse},
+    routing::{get, post},
+    Json, Router,
 };
-use axum::{response::Html,routing::{get, post}, Router};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use tower_http::cors::{Any, CorsLayer};
+
+#[derive(Debug, Deserialize)]
+pub struct LoginUser {
+    pub user_id: String,
+    // pub user_pw: String,
+}
+
 pub async fn index() -> Html<&'static str> {
-    println!("{}",1);
+    println!("{}", 1);
     let html_content = include_str!("../index.html");
     Html(html_content)
 }
+
+pub async fn index2(input: Json<LoginUser>) -> JsonResponse<Value> {
+    println!("{:?}", input);
+    JsonResponse(json!({ "result": 1}))
+}
 pub async fn run() {
+    let cors = CorsLayer::new()
+        .allow_methods(Any)
+        .allow_headers(Any)
+        .allow_origin(Any);
+
     let app = Router::new()
         // .route("/auth/users", post(get_users))
         // .route("/auth/join", post(join))
@@ -20,10 +39,11 @@ pub async fn run() {
         // .route("/auth/secession", post(secession))
         // .route("/auth/update", post(update_user))
         // .route("/test/test", post(test));
-    .route("/", get(index));
+        .route("/ip", post(index2))
+        .route("/", get(index))
+        .layer(cors);
 
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
         .unwrap();
     // 나머지 서버 설정
